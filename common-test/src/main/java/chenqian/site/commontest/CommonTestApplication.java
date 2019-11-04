f75ff8461e06fe4834972a4bb69ece1e42c02e8c
chenqian <992091149@qq.com>
2019-11-04 15:13:31 +0800

    init

---
+++ common-test/src/main/java/chenqian/site/commontest/CommonTestApplication.java
@@ -0,0 +1,13 @@
+package chenqian.site.commontest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonTestApplication.class, args);
+    }
+
+}
